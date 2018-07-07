af52f4e034fc683e20de383e750ced7ae49643f0
1803067851@qq.com <1803067851@qq.com>
2018-07-07 09:51:25 +0800

    modify

--- install_libvmi.sh
+++ install_libvmi.sh
@@ -1,8 +1,8 @@
 echo "============================begin install libvmi============================"
 sleep 2
 cd pdbparse && python setup.py install && cd ..
-cd json-c && sh autogen.sh && ./configure && make && make install && make check && cd ..
-cd libvmi && ./autogen.sh && ./configure -build=x86_64-linux-gnu --disable-xen && make && make install && ldconfig && cd ..
+cd json-c && chmod u+x *.sh && sh autogen.sh && ./configure && make && make install && cd ..
+cd libvmi && chmod u+x *.sh && ./autogen.sh && ./configure -build=x86_64-linux-gnu --disable-xen && make && make install && ldconfig && cd ..
 
 echo "============================libvmi install end============================"
 sleep 2
--- install_volatility.sh
+++ install_volatility.sh
@@ -1,5 +1,6 @@
 echo "===========================volatility install begin==================================================="
 sleep 2
+tar zxvf 2.6.tar.gz && mv volatility-2.6 volatility
 pip install PyCrypto Distorm3 ujson openpyxl IPython && easy_install --upgrade pytz
 cd libforensic1394 && mkdir build && cd build && cmake -G"Unix Makefiles" ../ && make && make install && cd ../../
 tar zxvf v3.7.1.tar.gz && cd yara-3.7.1 && ./bootstrap.sh && ./configure && make && make install && ldconfig && cd ..
